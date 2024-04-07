5527a6e6ecff8daf68e3c1a9868d2a772f914ad7
lightgleb <prorocker7@gmail.com>
2024-04-07 16:31:01 +0300

    Добавлены проверки на body

--- tests/test_get_user.py
+++ tests/test_get_user.py
@@ -16,6 +16,27 @@ def test_get_user_schema_validate():
         validate(body, schema=json.loads(file.read()))
 
 
+def test_objects_from_request_returns_in_response():
+    id = 1
+    email = "george.bluth@reqres.in"
+    first_name = "George"
+    last_name = "Bluth"
+    avatar = "https://reqres.in/img/faces/1-image.jpg"
+    url = "https://reqres.in/#support-heading"
+    text = "To keep ReqRes free, contributions towards server costs are appreciated!"
+
+    response = requests.get("https://reqres.in/api/users/1")
+    body = response.json()
+
+    assert body["data"]["id"] == id
+    assert body["data"]["email"] == email
+    assert body["data"]["first_name"] == first_name
+    assert body["data"]["last_name"] == last_name
+    assert body["data"]["avatar"] == avatar
+    assert body["support"]["url"] == url
+    assert body["support"]["text"] == text
+
+
 def test_user_not_found():
     response = requests.get("https://reqres.in/api/users/10000")
 
--- tests/test_put_user.py
+++ tests/test_put_user.py
@@ -14,3 +14,15 @@ def test_put_user_schema_validate():
     assert response.status_code == 200
     with open(schema) as file:
         validate(body, schema=json.loads(file.read()))
+
+
+def test_name_job_from_request_returns_in_response():
+    name = "morpheus"
+    job = "leader"
+
+    response = requests.put("https://reqres.in/api/users/5",
+                            data={"name": {name}, "job": {job}})
+    body = response.json()
+
+    assert body["name"] == name
+    assert body["job"] == job
